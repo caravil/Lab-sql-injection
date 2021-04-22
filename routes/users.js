777f334115482b1b210a12ba51f49a74685db9fa
carlosandressanchezaviles <carlos.sanchez7@udea.edu.co>
2021-04-21 19:40:40 -0500

    Lab-sql-injection
    Laboratorio de inyección de SQL

---
+++ routes/users.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET users listing. */
+function usersRouter(connection, db) {
+  router.get('/', async function (req, res, next) {
+
+    // const connection = await connect();
+
+    const query = req.query.id ? 
+    `select first_name, last_name from users where id = ? and role = 'Vendedor'`
+    : `select first_name, last_name from users where role = 'Vendedor'`
+
+    // query to database. Mysql and Oracle modules have different ways to query, this is why the if is needed.
+    if (db === 'oracle') {
+      try {
+        const results = await connection.execute(query, []);
+        res.send(results.rows);
+      } catch (err) {
+        console.log('Ouch!', err)
+      }
+    } else {
+      const request = connection.query(query,
+        [
+          req.body.id = req.query.id
+        ],
+        (error, results, fields) => {
+        if (error) throw error;
+        res.send(results);
+      })
+      console.log(request.sql)
+    }
+  });
+  return router;
+}
+module.exports = usersRouter;
